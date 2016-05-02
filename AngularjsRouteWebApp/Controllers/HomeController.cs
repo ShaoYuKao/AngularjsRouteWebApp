09f9f5af2a37deccad9917d58802e258e9d85d36
shaoYu <shaoyu.kao@gmail.com>
2016-05-02 13:04:18 +0800

    2016/05/02 問題 ASP.NET SignalR 與 AngularJS - ngRoute 與 ngView 結合當切換頁面時，會出現 Angular 中 controller 的 model 資料 與 View model 無法雙向繫結

---
+++ AngularjsRouteWebApp/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AngularjsRouteWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult NgRoute()
+        {
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetJson()
+        {
+            Person person = new Person();
+            person.Name = "Kitty";
+            person.Age = 18;
+
+            return Json(person);
+        }
+
+        public ActionResult SendMessage()
+        {
+            return View();
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ AngularjsRouteWebApp/Scripts/Routing.js
@@ -0,0 +1,65 @@
+﻿angular.module('MyApp', ['ngRoute']) //extending from previously created angularjs module in part1
+// here ['ngRoute'] is not required, I have just added to make you understand in a single place
+.config(function ($routeProvider, $locationProvider) {
+    //here we will write code for implement routing 
+    $routeProvider
+    .when('/', { // This is for reditect to another route
+        redirectTo: function () {
+            return '/home';
+        }
+    })
+    .when('/home', {
+        templateUrl: '/Template/Home.html',
+        controller: 'HomeController'
+    })
+    .when('/about', {
+        templateUrl: '/Template/About.html',
+        controller: 'AboutController'
+    })
+    .when('/order/:id', {
+        templateUrl: '/Template/Order.html',
+        controller: 'OrderController'
+    })
+    .otherwise({   // This is when any route not matched
+        templateUrl: '/Template/Error.html',
+        controller: 'ErrorController'
+    })
+
+    $locationProvider.html5Mode(false).hashPrefix('!'); // This is for Hashbang Mode
+})
+.controller('HomeController', ["$rootScope", "$scope", "$http", function ($rootScope, $scope, $http) {
+    $scope.Message = "This is HOME page";
+    $scope.jsonData = [];
+    $scope.data = [];
+
+    $http({
+        method: "post",
+        url: window.location.origin + "/Home/GetJson"
+    }).success(function (data) {
+        $scope.jsonData = data;
+        console.log(data);
+    }).error(function (err) { });
+
+    $scope.myHub = $.connection.myHub; //chatHub小寫
+
+    $.connection.hub.start().done(function () {
+        $scope.myHub.server.hello();
+    });
+
+    $scope.myHub.client.show = function (msg) {
+        $scope.data.push(msg);
+        console.log($scope.data);
+        $rootScope.$apply();//更新
+    };
+
+}])
+.controller('AboutController', function ($scope) {
+    $scope.Message = "This is ABOUT page";
+})
+.controller('OrderController', function ($scope, $routeParams) {
+    // $routeParams used for get query string value
+    $scope.Message = "This is ORDER Page with query string id value " + $routeParams.id;
+})
+.controller('ErrorController', function ($scope) {
+    $scope.Message = "404 Not Found!";
+});
